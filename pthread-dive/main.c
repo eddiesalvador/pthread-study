827ff24bbbb0996a8841e91a7b66d33c1ce63ed1
eddiesalvador <71903910+eddiesalvador@users.noreply.github.com>
2024-06-05 22:17:18 -0400

    Initial Commit

---
+++ pthread-dive/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  pthread-dive
+//
+//  Created by Eddie Salvador on 6/5/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
